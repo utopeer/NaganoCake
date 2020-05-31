b76f9f64dfcb373ef3fcb019ff3a2ba6e1252ba7
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-31 23:54:25 +0900

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,48 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+      case resource
+      when Admin
+      admin_admins_path
+      when Member
+      root_path
+      end
+    end
+
+	def after_sign_up_path_for(resource)
+      case resource
+      when Admin
+      admin_admins_path
+      when Member
+      root_path
+      end
+    end
+
+    # def after_sign_out_path_for(resource)
+    #   case resource
+    #   when Admin
+    #   new_admin_admin_session_path
+    #   when Member
+    #   new_public_member_session_path
+    #   end
+    # end
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :admin
+      new_admin_admin_session_path
+      else
+      root_path
+      end
+    end
+
+
+
+	protected
+
+	def configure_permitted_parameters
+		added_attrs = [ :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :password, :password_confirmation ]
+
+		devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+	end
 end
