92c39327f746f836b3e0c2f73a37cac15dedd18b
nyaaaaaan0129 <63774387+nyaaaaaan0129@users.noreply.github.com>
2020-05-31 15:02:58 +0900

    Revert "Revert 4 model relation"

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,19 @@
+class Public::AddressesController < ApplicationController
+	def new
+	end
+	
+    def index
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	def create
+	end
+
+	def update
+	end
+end
