cead95c635054ebee1fb845d791b4802a01d46e8
mwataru-pro <m1130wata@gmail.com>
2020-05-31 17:01:30 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    devise_for :admins
+
+    resources :items
+    resources :genres
+    resources :members
+    resources :orders
+    resources :order_items, only:[:update]
+
+    get '/admins' => 'admin#top'
+  end
+
+  namespace :public do
+  
+    devise_for :members
+    
+    get '/about' => 'homes#about'
+
+    resources :items, only:[:index,:show]
+    resources :cart_items
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only:[:new,:create,:index,:show]
+    get '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    resource :members
+    patch '/members/withdrawal' => 'members#withdrawal'
+    resources :addresses
+  end
+
+ root 'homes#top'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_100758) do
+ActiveRecord::Schema.define(version: 2020_05_31_044515) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -101,6 +106,5 @@ ActiveRecord::Schema.define(version: 2020_05_30_100758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
 end
