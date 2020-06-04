28cb44faa597301067cdc79e8a67016c0b5e5404
vonxar <p02d1108@gmail.com>
2020-06-04 14:47:08 +0900

    終わりーーーーーーー

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,18 +2,18 @@ class Admin::OrderItemsController < ApplicationController
 
 	def update
 		@order_item = OrderItem.find(params[:id])
-
+		@order = @order_item.order
 		# @order_items = OrderItem.all
 		@order_item.update(order_item_params)
 		# @order_item = Order.OderItem.find(params[:id])
 		# @order_item.update(order_item_params)
 
 		if @order_item.making_status == "製作中"
-			@order_item.order.update(order_status: 2)
-		elsif @order_item.making_status == "製作完了"
-			@order_item.order.update(order_status: 3)
+			@order.update(order_status: 2)
+		elsif @order.order_items.count ==  @order.order_items.where(making_status: "製作完了").count
+			@order.update(order_status: 3)
 		end
-		redirect_to admin_order_path(@order_item)
+		redirect_to admin_order_path(@order_item.order)
 	end
 
 
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,6 +7,7 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @addresses = Address.all
   end
 
   def confirm
