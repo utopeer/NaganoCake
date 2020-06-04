58f85a8ab8dd81d7e73450999a9717721984e244
mwataru-pro <m1130wata@gmail.com>
2020-06-04 23:51:06 +0900

    Add 会員商品一覧/詳細/トップ,その他デザイン変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,6 +5,10 @@ class Public::CartItemsController < ApplicationController
 	end
 
 	def create
+		@cart_item = CartItem.new(cart_item_params)
+		@cart_item.member_id = current_member.id
+		@cart_item.save
+		redirect_to public_cart_items_path
 	end
 
 	def update
@@ -14,5 +18,10 @@ class Public::CartItemsController < ApplicationController
 	end
 
 	def destroy_all
+	end
+
+	private
+	def cart_item_params
+		params.require(:cart_item).permit(:item_id, :number_of_item,:member_id)
 	end
 end
