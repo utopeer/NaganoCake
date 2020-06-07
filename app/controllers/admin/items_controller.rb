3c2b4a43836f76a4b08d94a7150c086662603874
mwataru-pro <m1130wata@gmail.com>
2020-06-07 13:12:09 +0900

    CSS修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,7 +13,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-  	@item = Item.create!(item_params)
+  	@item = Item.new(item_params)
   	if @item.save
   	   redirect_to admin_item_path(@item)
   	else
