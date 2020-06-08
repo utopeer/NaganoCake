f0ce07bd1a3cb6b2b6131d8886fa42e97ad35038
mwataru-pro <m1130wata@gmail.com>
2020-06-08 11:38:58 +0900

    修正加筆しました

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8) # ページネーションの追加/8件ごとにページ作成
+    @items = Item.where(sale_status: 0).page(params[:page]).per(8) # where文で販売ステータスが販売中のものだけ表示させる/ページネーションの追加/8件ごとにページ作成
     @quantity = Item.count # quantity => 総量/.count =>カウントメソッド、配列の要素の数を数えるための機能
     @genres = Genre.where(valid_invalid_status: 0) # ジャンルの有効無効ステータスが有効のものだけ探す/除外検索
   end
