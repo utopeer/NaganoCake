3cb8357172d2cc123b8c6a20602a5f864b5238eb
mwataru-pro <m1130wata@gmail.com>
2020-06-10 16:19:22 +0900

    住所検索機能を追加しました

--- app/models/member.rb
+++ app/models/member.rb
@@ -15,6 +15,19 @@ class Member < ApplicationRecord
   # バリデーション
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, presence: true
 
+  # 住所検索用 その他記述はhtmlとjavascripts/public/members.coffeeに記載
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
+  # 退会
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
