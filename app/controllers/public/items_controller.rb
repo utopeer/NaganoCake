f71ada32f14ac5cff4c083999c3b782ea3c1d02f
mwataru-pro <m1130wata@gmail.com>
2020-06-13 14:55:41 +0900

    Update ジャンル検索後の一覧件数

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -14,7 +14,7 @@ class Public::ItemsController < ApplicationController
   # ジャンル検索機能
   def search
     @items = Item.where(genre_id: params[:format]).page(params[:page]).per(8) # パラメーターで渡ってきたジャンルidを元に、Item内のgenre_idと完全一致する商品情報を取得している。
-    @quantity = Item.count
+    @quantity = Item.where(genre_id: params[:format]).count # 検索してきたジャンルの商品数をカウント
     @genres = Genre.where(valid_invalid_status: 0)
     render 'index' # renderを使用してviewファイルを表示したときにはactionを呼び出し処理をしているわけではないため、上記のように必要な変数を用意しておく必要がある、
   end
