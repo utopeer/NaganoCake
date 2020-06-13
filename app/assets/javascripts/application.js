6f18cb58f11bb7ddc6b3a5c362edd5282aba28a2
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-06-13 15:35:25 +0900

    again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -86,4 +86,27 @@ $(function(){
 
 
 
+$(function() {
+  var h = $(window).height();
+  
+  $('#wrap').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+});
+  
+$(window).load(function () { 
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+  $('#wrap').css('display', 'block');
+});
+  
+$(function(){
+  setTimeout('stopload()',10000);
+});
+  
+function stopload(){
+  $('#wrap').css('display','block');
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+}
+
 
